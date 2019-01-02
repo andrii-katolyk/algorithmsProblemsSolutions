0b4d22d3e7b43a1a2e8624a18cb40ebb992e5376
andrii-katolyk <katholyk.a@gmail.com>
2019-01-02 14:21:05 +0200

    Fibonachi numbers challenge sollution

---
+++ Algorithms.Solutions/BigFibonachiNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Solutions
+{
+	public class BigFibonachiNumbers
+	{
+		//get Fibonachi numbers under some limit. Calculate the sum of even-valued Fibonachi numbers.
+
+		public List<int> GetFibonachiSequence(int limit)
+		{
+			var fibonachiSequence = new List<int> { 1, 2 };
+
+			int index = 1;
+			int newValue = 3;
+
+			while(newValue <= limit)
+			{
+				fibonachiSequence.Add(newValue);
+				index++;
+				newValue += fibonachiSequence[index - 1];
+			}
+
+			return fibonachiSequence;
+		}
+
+		public int GetEvenFibonachiNumbersSum(int limit)
+		{
+			int evenNumbersSum = 0;
+			int previousValue = 1;
+			int currentValue = 2;
+
+			while (currentValue <= limit)
+			{
+				if(currentValue % 2 == 0)
+				{
+					evenNumbersSum += currentValue;
+				}
+
+				int val = currentValue;
+				currentValue += previousValue;
+				previousValue = val;
+			}
+
+			return evenNumbersSum;
+		}
+	}
+}
---
+++ Algorithms.Tests/FibonachiTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Solutions;
+
+namespace Algorithms.Tests
+{
+	[TestClass]
+	public class FibonachiTests
+	{
+		[TestMethod]
+		public void Sequence_GetElementsBelow_100()
+		{
+			BigFibonachiNumbers fibonachi = new BigFibonachiNumbers();
+			var sequence = fibonachi.GetFibonachiSequence(100);
+			Assert.IsTrue(sequence.Count == 10);
+		}
+
+		[TestMethod]
+		public void Sum_GetSumOfEvenElementsBelow_100()
+		{
+			BigFibonachiNumbers fibonachi = new BigFibonachiNumbers();
+			var sum = fibonachi.GetEvenFibonachiNumbersSum(100);
+			Assert.AreEqual(44, sum);
+		}
+
+		[TestMethod]
+		public void Sum_GetSumOfEvenElementsBelow_4000000()
+		{
+			BigFibonachiNumbers fibonachi = new BigFibonachiNumbers();
+			var sum = fibonachi.GetEvenFibonachiNumbersSum(4000000);
+			Assert.AreEqual(4613732, sum);
+		}
+	}
+}
